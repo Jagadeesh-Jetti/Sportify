086b8e856b250dd2f25178d0bc639567e4a58840
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-09-17 22:57:22 +0530

    chore: updated name

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 app.get("/", (req, res) => {
-  res.send("Sport Finder Backend");
+  res.send("Sportify Backend");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Sport finder backend started");
+  console.log("Sportify backend started");
 });

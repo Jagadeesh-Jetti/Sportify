c7ff3b59ca42d60b0c5444294b2fc9e7cda8f528
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-09-17 22:49:29 +0530

    feat: initialized express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Sportify Backend");
+const express = require("express");
+
+app.get("/", (req, res) => {
+  res.send("Sport Finder Backend");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Sport finder backend started");
+});

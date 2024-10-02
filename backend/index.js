f7e874b563eee33221dec0f9730f3bdb4f7c5647
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-02 15:43:42 +0530

    refactor: addeed routers to index file

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 
 const dataBase = require("./db");
+const BookingRouter = require("./routers/booking.router");
+const OwnerRouter = require("./routers/owner.router");
+const PlayerRouter = require("./routers/player.router");
+const VenueRouter = require("./routers/venue.router");
+
+app.use(express.json());
 
 dataBase();
+
+app.use("/bookings", BookingRouter);
+app.use("/owner", OwnerRouter);
+app.use("/player", PlayerRouter);
+app.use("/venue", VenueRouter);
 
 app.get("/", (req, res) => {
   res.send("Sportify Backend");

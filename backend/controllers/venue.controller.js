c2587d5f8b3035ea9e28c2efaa86eaf5cacf424d
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-02 15:42:54 +0530

    feat: completed CRUD for venue controller

---
+++ backend/controllers/venue.controller.js
@@ -0,0 +1,56 @@
+const Venue = require("../models/venue.model");
+
+const createVenue = async (req, res) => {
+  try {
+    const venue = await new Venue(req.body);
+
+    res.status(201).json({ message: "Venue created successfully", venue });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const getVenueById = async (req, res) => {
+  try {
+    const venue = await Venue.findById(req.params.id);
+
+    if (!venue) {
+      return res.status(404).json({ message: "Venue not found" });
+    }
+    res.status(200).json({ message: "Venue fetched successfully", venue });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const updateVenue = async (req, res) => {
+  try {
+    const venue = await Venue.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!venue) {
+      return res.status(404).json({ message: "Venue not found" });
+    }
+
+    res.status(200).json({ message: "Venue updated successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const deleteVenue = async (req, res) => {
+  try {
+    const venue = await Venue.findByIdAndDelete(req.params.id);
+
+    if (!venue) {
+      return res.status(404).json({ message: "Venue not found" });
+    }
+
+    res.status(200).json({ message: "Venue deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { createVenue, getVenueById, updateVenue, deleteVenue };

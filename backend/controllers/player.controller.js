4b9245d86a37dc77e8541cb839ff56f38997e2e6
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-06 22:10:58 +0530

    feat: added authentication

--- backend/controllers/player.controller.js
+++ backend/controllers/player.controller.js
@@ -1,4 +1,12 @@
 const Player = require("../models/player.model");
+const jwt = require("jsonwebtoken");
+const dotEnv = require("dotenv");
+
+dotEnv.config({
+  path: ".env",
+});
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const createPlayer = async (req, res) => {
   try {
@@ -20,20 +28,13 @@ const loginPlayer = async (req, res) => {
       return res.status(404).json({ message: "Player not found" });
     }
 
-    if (player.password !== password) {
+    const isMatch = await player.comparePassword(password);
+    if (!isMatch) {
       return res.status(401).json({ message: "Invalid password" });
     }
 
-    res.status(200).json({ message: "Login successful", player });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-};
-
-const getAllPlayers = async (req, res) => {
-  try {
-    const players = await Player.find();
-    res.status(200).json({ message: "Players fetched successfully", players });
+    const token = jwt.sign({ email: player.email }, JWT_SECRET);
+    res.status(200).json({ message: "Login successful", player, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
@@ -44,7 +45,7 @@ const getPlayerProfile = async (req, res) => {
     const player = await Player.findById(req.params.id);
 
     if (!player) {
-      return res.status(404).json({ message: "Requested player not found" });
+      return res.status(404).json({ message: "Player profile not found" });
     }
     res
       .status(200)
@@ -61,7 +62,7 @@ const updatePlayerProfile = async (req, res) => {
     });
 
     if (!player) {
-      res.status(404).json({ message: "Player not found to update" });
+      return res.status(404).json({ message: "Player not found to update" });
     }
     res
       .status(200)
@@ -76,7 +77,7 @@ const deletePlayerProfile = async (req, res) => {
     const player = await Player.findByIdAndDelete(req.params.id);
 
     if (!player) {
-      res.status(404).json({ message: "Player not found to delete" });
+      return res.status(404).json({ message: "Player not found to delete" });
     }
     res.status(200).json({ message: "Player deleted successfully" });
   } catch (error) {
@@ -87,7 +88,6 @@ const deletePlayerProfile = async (req, res) => {
 module.exports = {
   createPlayer,
   loginPlayer,
-  getAllPlayers,
   getPlayerProfile,
   updatePlayerProfile,
   deletePlayerProfile,

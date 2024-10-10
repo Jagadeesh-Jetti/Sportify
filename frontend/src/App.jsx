c8660629d452a37139115ff9abdff11e1ff2b95d
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-10 12:38:55 +0530

    feat: basic react and tailwind setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl text-red-500 p-5 text-center">
+        Sportify Landing Page
+      </div>
+    </>
+  );
+}
+
+export default App;

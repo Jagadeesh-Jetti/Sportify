5358d006d62df2d6afc037aa9d9ae39cf899a478
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-11 23:06:47 +0530

    feat: created react app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import { Navbar } from "./components/layouts-components/Navbar";
+import { Signup } from "./components/modals-components/signup";
 
 function App() {
   return (
     <>
+      <Navbar />
       <div className="text-3xl text-red-500 p-5 text-center">
         Sportify Landing Page
       </div>
+      <Signup />
     </>
   );
 }

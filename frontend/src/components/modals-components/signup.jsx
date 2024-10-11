22c7c4fbbeab76e77f51ef6ca7619ad369f2f3e7
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-11 23:01:29 +0530

    feat: created signup component

---
+++ frontend/src/components/modals-components/signup.jsx
@@ -0,0 +1,8 @@
+export const Signup = () => {
+  return (
+    <div className="flex flex-col bg-gray-200 w-72 p-5 justify-center place-content-center">
+      <div> Email</div>
+      <div> Password</div>
+    </div>
+  );
+};

a8f09eb7e197da8f4c01d529c0db282b6195f724
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-11 23:06:07 +0530

    feat: created navbar component

---
+++ frontend/src/components/layouts-components/Navbar.jsx
@@ -0,0 +1,10 @@
+export const Navbar = () => {
+  return (
+    <div className="flex justify-between p-4 px-6 align-center bg-gray-200">
+      <div className="text-3xl font-bold text-blue-500"> Sportify </div>
+      <div> Search Location </div>
+      <div className="text-xl"> Book </div>
+      <div className="text-xl"> Signup/Login</div>
+    </div>
+  );
+};

2de5a72b282287f51f10b68a233ab07999724ed2
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-15 23:36:08 +0530

    feat: added router and routes to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import { Navbar } from "./components/layouts-components/Navbar";
 import { Login } from "./components/modals-components/Login";
+import { Signup } from "./components/modals-components/Signup";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="bg-white">
       <Navbar />
-
-      <Login />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   );
 }

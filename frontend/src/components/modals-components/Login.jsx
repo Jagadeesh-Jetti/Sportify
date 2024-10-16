ea88ef56ba2e138e360351bf40e8077d3a4b0115
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-10-16 21:54:04 +0530

    style: updated styles for login

--- frontend/src/components/modals-components/Login.jsx
+++ frontend/src/components/modals-components/Login.jsx
@@ -1,35 +1,37 @@
 export const Login = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
-      <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-        <h1 className="font-bold text-center text-3xl mb-5 text-indigo-600">
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center sm:py-12">
+      <div className=" bg-white shadow-lg rounded-lg p-8 xs:p-0  mx-auto md:w-full md:max-w-md">
+        <h1 className="font-bold text-center text-3xl mb-8 text-indigo-600 ">
           {/* Sportify */}
         </h1>
-        <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
-          <div className="px-5 py-7">
-            <h2 className="text-2xl font-bold text-center mb-6">
+
+        <div className="bg-white rounded-lg divide-y divide-gray-200">
+          <div className="px-5 py-7 ">
+            <h2 className="text-3xl font-bold text-center mb-8">
               Login to Your Account
             </h2>
+
             <form className="space-y-6">
-              <div className="space-y-1">
-                <label className="block text-sm font-medium text-gray-600">
+              <div>
+                <label className="block text-lg font-medium text-gray-600">
                   Email Address
                 </label>
                 <input
                   type="email"
                   name="email"
-                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-indigo-500 focus:border-indigo-500"
+                  className="mt-2 w-full p-3 border border-gray-300 rounded-lg focus:ring-indigo-500 focus:border-indigo-500"
                   placeholder="Enter your email"
                 />
               </div>
               <div className="space-y-1">
-                <label className="block text-sm font-medium text-gray-600">
+                <label className="block text-lg font-medium text-gray-600">
                   Password
                 </label>
                 <input
                   type="password"
                   name="password"
-                  className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-indigo-500 focus:border-indigo-500"
+                  className="mt-2 w-full p-3 border border-gray-300 rounded-lg focus:ring-indigo-500 focus:border-indigo-500"
                   placeholder="Enter your password"
                 />
               </div>
@@ -65,6 +67,7 @@ export const Login = () => {
               </button>
             </form>
           </div>
+
           <div className="py-5">
             <div className="text-center">
               <p className="text-gray-600">
